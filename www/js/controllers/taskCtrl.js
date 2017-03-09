2be09c7b56eb3f71bf97725ecef7b85dd889cde3
Corentin Chateau <Corentin.Chateau.49@gmail.com>
2017-03-09 13:58:47 +0100

    Check + Supression tasks

--- www/js/controllers/taskCtrl.js
+++ www/js/controllers/taskCtrl.js
@@ -1,17 +1,31 @@
 angular.module('plumo.controllers')
 
 .controller('TasksCtrl', function ($scope, Room, Task) {
-    $scope.house={};
-    Task.getUserTasks().then(function(tasks){
-        $scope.tasks=tasks.data;
-        console.log(tasks);
-    })
-    Room.getHouse().then(function(house){
-        $scope.house=house;
-    })
+	$scope.house={};
 
-    $scope.$on('cloud:push:notification', function (event, data) {
-        var msg = data.message;
-        alert(msg.title + ': ' + msg.text);
-    });
+
+	Task.getUserTasks().then(function(tasks){
+		$scope.tasks=tasks.data;
+		console.log(tasks);
+	})
+	Room.getHouse().then(function(house){
+		$scope.house=house;
+	})
+
+	$scope.$on('cloud:push:notification', function (event, data) {
+		var msg = data.message;
+		alert(msg.title + ': ' + msg.text);
+	});
+
+	$scope.performTask = function (task) {
+		Task.performTask(task).then(function (response) {
+			task.done = response.data.done;
+		});
+	};
+
+	$scope.deleteTask = function (task) {
+		Task.deleteTask(task).then(function (response) {
+			_.remove($scope.tasks, {'id' : task.id});
+		});
+	};
 });
--- www/js/services/taskService.js
+++ www/js/services/taskService.js
@@ -1,15 +1,18 @@
 angular.module('plumo.services')
 
 .factory('Task', function(HttpClient){
-    return {
-        saveTask : function(task){  
-            return HttpClient.post('/task',task);  
-        },
-        getUserTasks: function () {
-            return HttpClient.get('/task');
-        },
-        deleteTask: function (task) {
-            return HttpClient.remove('/task/'+task.id);
-        }
-    };
+	return {
+		performTask: function (task) {
+			return HttpClient.get('/task/perform/' + task.id);
+		},
+		saveTask : function(task){  
+			return HttpClient.post('/task',task);  
+		},
+		getUserTasks: function () {
+			return HttpClient.get('/task');
+		},
+		deleteTask: function (task) {
+			return HttpClient.remove('/task/'+task.id);
+		}
+	};
 });
